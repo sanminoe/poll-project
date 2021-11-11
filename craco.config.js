ecbfe6736aadcd0bbf53e4fa5ed1376319aa2e96
sanminoe <megasantiago132@gmail.com>
2021-11-11 13:10:37 +0100

    build modification

--- craco.config.js
+++ craco.config.js
@@ -3,5 +3,13 @@ module.exports = {
 		postcss: {
 			plugins: [ require('tailwindcss'), require('autoprefixer') ]
 		}
+	},
+	webpack: {
+		plugins: [
+			new webpack.DefinePlugin({
+				NODE_ENV: JSON.stringify(process.env.NODE_ENV),
+				REACT_APP_API_KEY: JSON.stringify(process.env.REACT_APP_API_KEY)
+			})
+		]
 	}
 };
